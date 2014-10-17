c36c5ee12e7b221bf965e8391e14d8a0d1a22d2e
spoonFu <xinyu2010@126.com>
2014-10-17 19:23:28 +0800

    exercise

--- src/com/invoke/InvocationHandlerImpl.java
+++ src/com/invoke/InvocationHandlerImpl.java
@@ -1,21 +1,37 @@
 package com.invoke;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/**
+ * 动态代理
+ * @author FuShaoxing
+ */
 public class InvocationHandlerImpl implements InvocationHandler {
 
 	private Object delegate;
 
+	/**
+	 * 返回一个代理类的对象
+	 * @param delegate
+	 * @return
+	 */
 	public Object bind(Object delegate) {
 		this.delegate = delegate;
-		return Proxy.newProxyInstance(delegate.getClass().getClassLoader(),
-				delegate.getClass().getInterfaces(), this);
+		ClassLoader cl = delegate.getClass().getClassLoader();
+		Class<?>[] intfce = delegate.getClass().getInterfaces();
+		return Proxy.newProxyInstance(cl, intfce, this);
 	}
 
+	/**
+	 *  生成代理对象被调用方法时 
+	 *  method为被调用方法
+	 *  args[]为传入参数
+	 *  proxy
+	 */
 	@Override
-	public Object invoke(Object proxy, Method method, Object[] args)
-			throws Throwable {
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		Object ret = null;
 		if (method.getName().startsWith("fsx")) {
 			System.out.println("before");
@@ -24,6 +40,7 @@ public class InvocationHandlerImpl implements InvocationHandler {
 		} else {
 			ret = method.invoke(this.delegate, args);
 		}
+		System.out.println(proxy.getClass().getName());
 		return ret;
 	}
 
--- src/com/invoke/Test.java
+++ src/com/invoke/Test.java
@@ -1,10 +1,13 @@
 package com.invoke;
+
+/**
+ * 动态代理
+ * @author FuShaoxing
+ */
 public class Test {
 	public static void main(String[] args) {
-		IService s = (IService) new InvocationHandlerImpl()
-				.bind(new ServiceImpl());
-		String str = s.fsx();
-		System.out.println(str);
+		IService s = (IService) new InvocationHandlerImpl().bind(new ServiceImpl());
+		s.fsx();
 	}
 
 }
